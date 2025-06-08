9a52eeb38218caf9af3c249f3d4bf2e7bffcf695
Olga <oyakubovska80@gmail.com>
2025-06-08 20:34:16 +0200

    hm8-001

--- src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -1,20 +1,31 @@
-import {UserType} from '../HW8'
+import { UserType } from "../HW8";
 
 type ActionType =
-    | { type: 'sort'; payload: 'up' | 'down' }
-    | { type: 'check'; payload: number }
+  | { type: "sort"; payload: "up" | "down" }
+  | { type: "check"; payload: number };
 
-export const homeWorkReducer = (state: any, action: any): any => { // need to fix any
-    switch (action.type) {
-        case 'sort': { // by name
-
-            return state // need to fix
-        }
-        case 'check': {
-
-            return state // need to fix
-        }
-        default:
-            return state
+export const homeWorkReducer = (
+  state: UserType[],
+  action: ActionType
+): UserType[] => {
+  // need to fix any
+  switch (action.type) {
+    case "sort": {
+      // by name
+      const sortedArray = [...state].sort((a, b) => {
+        return action.payload === "up"
+          ? a.name.localeCompare(b.name)
+          : b.name.localeCompare(a.name);
+      });
+      return sortedArray; // need to fix
     }
-}
+    case "check": {
+      const sortedArray = [...state].filter(
+        (item) => item.age > action.payload
+      );
+      return sortedArray; // need to fix
+    }
+    default:
+      return state;
+  }
+};
